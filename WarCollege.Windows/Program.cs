a84889dc2bd840201ba5b9ce50fbbac643a0a077
James Allred <wildj79@gmail.com>
2017-07-21 16:58:46 -0500

    Add windows project.

---
+++ WarCollege.Windows/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Autofac;
+
+namespace WarCollege.Windows
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var bootstrapper = new IocBootstraper();
+            var container = bootstrapper.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<Eto.Forms.Application>();
+                app.Run();
+            }
+        }
+    }
+}
